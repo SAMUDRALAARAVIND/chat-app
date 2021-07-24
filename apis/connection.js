ebad794de684b0a19d6114ce1e5f43cab6c5ba2c
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2021-07-24 11:19:53 +0530

    removed credentials

--- apis/connection.js
+++ apis/connection.js
@@ -2,10 +2,10 @@ const mysql = require("mysql")
 
 
 const connection = mysql.createConnection({
-    host:"bn7pkibeymz3uochbr3v-mysql.services.clever-cloud.com",
-    user:"ubp0x7wg4d6yioc8",
-    password:"5uxsJsdystxi59TLQwO2",
-    database:"bn7pkibeymz3uochbr3v",
+    host:"**********************",
+    user:"****************",
+    password:"**************",
+    database:"*****************",
     multipleStatements: true
 })
 
